f6f9a951dc32d2388549ca3c99649659321c830f
sagartmohite <sagarmohite.test@gmail.com>
2024-04-23 15:00:31 +0530

    first commit

---
+++ src/main/java/com/thetestingacademy/endpoints/APIConstants.java
@@ -0,0 +1,9 @@
+package com.thetestingacademy.endpoints;
+
+public class APIConstants {
+
+    // public static String BASE_URL  = UtilsExcel.fetchDataFromXLSX("Sheet1","BaseUrl","Value");
+    public static String BASE_URL  = "https://restful-booker.herokuapp.com";
+    public static String CREATE_UPDATE_BOOKING_URL  = "/booking";
+
+}
---
+++ src/test/java/com/thetestingacademy/tests/e2e/TC1.java
@@ -0,0 +1,5 @@
+package com.thetestingacademy.tests.e2e;
+
+public class TC1 {
+    // Create Booking -> Update Booking -> Delete Booking
+}
